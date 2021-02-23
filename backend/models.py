0c3b4d92473d6ab6499072a5f7c06c04c4991400
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-23 14:21:28 +0200

    connect to local development db

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,10 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = "trivia_dev"
+database_username = os.getenv('DATABASE_USERNAME')
+database_password = os.getenv('DATABASE_PASSWORD')
+database_path = "postgres://{}:{}@{}/{}".format(database_username,database_password,'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
