0d8685c0415b40636c89078e1bc248d373ee90f7
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-25 21:15:39 +0200

    test get_question endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,30 +4,39 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import Question, Category
+from config import DatabaseUri
 
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
-        """Define test variables and initialize app."""
         self.app = create_app()
+        self.database_uri = DatabaseUri()
+        self.database_uri.name = "trivia_test"
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = str(self.database_uri)
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
-            # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
+
+    def test_get_questions(self):
+        res = self.client().get('/questions?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['current_category'], None)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
 
     """
     TODO
