e66c4526a0b46198714770d45c5de5f50101dee4
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-25 23:33:31 +0200

    implement some tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -60,6 +60,48 @@ class TriviaTestCase(unittest.TestCase):
         response_json = res.json
         self.assertEqual(len(response_json['categories']),6)
 
+    def test_get_questions_returns_404(self):
+        res = self.client().get('/questions?page=100')
+        self.assertEqual(res.status_code, 404)
+
+    def test_422_delete_questions(self):
+        res = self.client().delete('/questions/1000')
+        self.assertEqual(res.status_code, 422)
+
+    def test_create_questions_response_with_201(self):
+        new_question = {
+            'question': 'new question',
+            'answer': 'new answer',
+            'category': 1,
+            'difficulty': 1,
+        }
+        res = self.client().post('/questions', json=new_question)
+        self.assertEqual(res.status_code, 201)
+
+    def test_500_create_questions(self):
+        invalid_question = {
+            'question': 'invalid question',
+            'answer': 'invalid answer',
+            'category': 'Not integer',
+            'difficulty': 'Not integer',
+        }
+        res = self.client().post('/questions', json=invalid_question)
+
+        self.assertEqual(res.status_code, 500)
+
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data['categories']))
+
+    def test_quizzes_return_422_when_pass_missing_parameter(self):
+        body = {}
+        res = self.client().post('/quizzes', json=body)
+        self.assertEqual(res.status_code, 422)
+
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
