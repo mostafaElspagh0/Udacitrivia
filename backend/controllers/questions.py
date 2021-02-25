2e458232712ffa0ba053115720604ca1c4b358dd
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-25 21:32:25 +0200

    fix questions does not return  404

--- backend/controllers/questions.py
+++ backend/controllers/questions.py
@@ -8,21 +8,18 @@ import sys
 def questions_controller(app, questions_per_page):
     @app.route('/questions', methods=['GET'])
     def questions():
-        try:
-            page = request.args.get('page', 1, type=int)
-            offset = questions_per_page * (page - 1)
-            all_categories = Category.query.all()
-            all_questions = Question.query.order_by(Question.id).limit(questions_per_page).offset(offset).all()
-            if len(all_questions) == 0:
-                abort(0)
-            return {
-                'categories': {category.id: category.type for category in all_categories},
-                'questions': [question.format() for question in all_questions],
-                'total_questions': Question.query.count(),
-                'current_category': None
-            }
-        except Exception:
-            abort(500)
+        page = request.args.get('page', 1, type=int)
+        offset = questions_per_page * (page - 1)
+        all_categories = Category.query.all()
+        all_questions = Question.query.order_by(Question.id).limit(questions_per_page).offset(offset).all()
+        if len(all_questions) == 0:
+            abort(404)
+        return {
+            'categories': {category.id: category.type for category in all_categories},
+            'questions': [question.format() for question in all_questions],
+            'total_questions': Question.query.count(),
+            'current_category': None
+        }
 
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_questions(question_id):
