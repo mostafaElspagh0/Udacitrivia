790cd4dcea8017a64c8a5e737a977e0f4b9b0240
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-25 23:05:49 +0200

    make quizzes controller return 422 when pass missing parameter

--- backend/controllers/quizzes.py
+++ backend/controllers/quizzes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models import Category, Question
 from sqlalchemy import and_
 
@@ -6,18 +6,19 @@ from sqlalchemy import and_
 def quizzes_controller(app):
     @app.route('/quizzes', methods=['POST'])
     def quizzes():
-        try:
-            payload = request.json
-            previous_questions_ids = payload['previous_questions']
-            category_id = payload['quiz_category']['id']
-            filters = [Question.id.notin_(previous_questions_ids)]
-            if category_id != 0:
-                filters.append(Question.category_id == category_id)
-            question = Question.query.filter(and_(*filters)).first()
-            return jsonify(
-                {
-                    'question': question.format() if question is not None else None
-                }
-            )
-        except Exception:
-            abort(500)
+        payload = request.json
+        if 'previous_questions' not in payload \
+                or 'quiz_category' not in payload \
+                or 'id' not in payload['quiz_category']:
+            abort(422)
+        previous_questions_ids = payload['previous_questions']
+        category_id = payload['quiz_category']['id']
+        filters = [Question.id.notin_(previous_questions_ids)]
+        if category_id != 0:
+            filters.append(Question.category_id == category_id)
+        question = Question.query.filter(and_(*filters)).first()
+        return jsonify(
+            {
+                'question': question.format() if question is not None else None
+            }
+        )
